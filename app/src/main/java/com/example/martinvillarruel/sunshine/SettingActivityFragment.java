63a8391cca0db0b68ff16ff8bd288baa7cb611c9
martin.villarruel <martin.villarruel@globant.com>
2016-01-29 17:41:53 -0300

    Initial commit

---
+++ app/src/main/java/com/example/martinvillarruel/sunshine/SettingActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.martinvillarruel.sunshine;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SettingActivityFragment extends Fragment {
+
+    public SettingActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_setting, container, false);
+    }
+}
